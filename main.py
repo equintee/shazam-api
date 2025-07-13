5ddeed578855f94a9899cbceca7f8228d79dad58
equintee <employes23@gmail.com>
2025-07-13 13:36:10 +0300

    bro exported wrong port lmao

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from service.music_recogniton_service import MusicRecognitionService
 def serve():
     server = grpc.server(thread_pool=futures.ThreadPoolExecutor(max_workers=10))
 
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:50001')
     add_MusicRecoginitonServiceServicer_to_server(MusicRecognitionService(), server)
     server.start()
-    print("Server started on port 50051")
+    print("Server started on port 50001")
     server.wait_for_termination()
 
 
